cf3034ed811b8a59e50e88d16c90437f554c50da
John Fristedt <john.fristedt@chas.se>
2016-12-05 10:25:21 +0100

    Json file upload

---
+++ MVC1/MVCCMS/Controllers/FileController.cs
@@ -0,0 +1,48 @@
+﻿using MVCCMS.Models.EntityModels;
+using MVCCMS.Models.ViewModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCMS.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UploadJson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadJson(FileUploadVM model)
+        {
+            //if (!ModelState.IsValid)
+            //    return View(model);
+
+            byte[] data;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                model.File.InputStream.CopyTo(ms);
+                data = ms.ToArray();
+            }
+
+            var json = Encoding.Default.GetString(data);
+
+            var entity = JsonConvert.DeserializeObject<Product[]>(json);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
